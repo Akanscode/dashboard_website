f796043b5009f69b7c0fc48a8c6fc72ad08157a6
Akanscode <akanscoded@gmail.com>
2024-01-23 16:00:18 +0100

    update

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -30,11 +30,11 @@ const Login = ({ setIsLoggedIn }) => {
             <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleLogin}>
               <div>
                 <label for="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                <input type="text" name="user" id="user" onChange={(e) => setUsername(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg  focus:border-gray-600 block w-full p-2.5 " placeholder="Username"/>
+                <input type="text" name="user" id="user" onChange={(e) => setUsername(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg  focus:border-gray-600 block w-full p-2.5 " placeholder="enter user for the username"/>
               </div>
               <div>
                 <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" id="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg  focus:border-gray-600 block w-full p-2.5 "/>
+                <input type="password" name="password" id="password" placeholder="enter password for the password" onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg  focus:border-gray-600 block w-full p-2.5 "/>
               </div>
              
               <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
